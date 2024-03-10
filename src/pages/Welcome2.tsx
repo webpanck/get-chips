60cfe6a2bafdb5c07ab34128da76bc6644abdc8b
webpanck <3500146682@qq.com>
2024-03-10 17:23:03 +0800

    添加第一个页面的 HTML

--- src/pages/Welcome2.tsx
+++ src/pages/Welcome2.tsx
@@ -1,10 +1,9 @@
-import { NavLink } from "react-router-dom"
+import { Link } from 'react-router-dom'
 
 export const Welcome2: React.FC = () => {
   return (
-    <div style={{ border: '1px solid red' }}>
-      2
-      <NavLink to="/welcome/3">下一页</NavLink>
+    <div>
+      2 <Link to="/welcome/3">下一页</Link>
     </div>
   )
 }
