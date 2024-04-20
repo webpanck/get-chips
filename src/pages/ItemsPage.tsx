f6cdb3fe1dc4f6f8606e6e60f5063e001db3d259
webpanck <3500146682@qq.com>
2024-04-20 23:49:20 +0800

    完成 ItemsList 组件，ItemsPage 页面基本完成

--- src/pages/ItemsPage.tsx
+++ src/pages/ItemsPage.tsx
@@ -5,6 +5,7 @@ import type { TimeRange } from '../components/TimeRangePicker'
 import { TimeRangePicker } from '../components/TimeRangePicker'
 import { AddItemFloatButton } from '../components/AddItemFloatButton'
 import { ItemsSummary } from './ItemsPage/ItemsSummary'
+import { ItemsList } from './ItemsPage/ItemsList'
 
 const Div = styled.div`
   background: linear-gradient(0deg, rgba(143,76,215,1) 0%, rgba(92,51,190,1) 100%);
@@ -12,6 +13,27 @@ const Div = styled.div`
 
 export const ItemsPage: React.FC = () => {
   const [timeRange, setTimeRange] = useState<TimeRange>('thisMonth')
+  const [items] = useState<Item[]>([
+    {
+      id: 1,
+      kind: 'incomes',
+      amount: 1000,
+      user_id: 1,
+      tag_ids: [1],
+      happen_at: '2021-01-01T00:00:00.000Z',
+      created_at: '2021-01-01T00:00:00.000Z',
+      updated_at: '2021-01-01T00:00:00.000Z',
+    }, {
+      id: 2,
+      kind: 'incomes',
+      amount: 1000,
+      user_id: 1,
+      tag_ids: [1],
+      happen_at: '2021-01-01T00:00:00.000Z',
+      created_at: '2021-01-01T00:00:00.000Z',
+      updated_at: '2021-01-01T00:00:00.000Z',
+    }
+  ])
   return (
     <div>
       <Div>
@@ -19,6 +41,7 @@ export const ItemsPage: React.FC = () => {
         <TimeRangePicker selected={timeRange} onSelected={setTimeRange} />
       </Div>
       <ItemsSummary />
+      <ItemsList items={items} />
       <AddItemFloatButton />
     </div>
   )
