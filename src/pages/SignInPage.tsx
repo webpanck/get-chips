e82ad299d4501b006f78d20e73cd201d398257a5
webpanck <3500146682@qq.com>
2024-06-22 20:07:00 +0800

    完成登录页面的样式

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -8,6 +8,26 @@ export const SignInPage: React.FC = () => {
       <Gradient>
         <TopNav title="登录" icon={<Icon name="back" />} />
       </Gradient>
+      <div text-center pt-40px pb-16px>
+        <Icon name="logo" className='w-64px h-68px' />
+        <h1 text-32px text="#7878FF" font-bold>山竹记账</h1>
+      </div>
+      <form c-form>
+        <div>
+          <span c-form-label>邮箱地址</span>
+          <input c-input-text type="text" placeholder='请输入邮箱，然后点击发送验证码' />
+        </div>
+        <div>
+          <span c-form-label>验证码</span>
+          <div flex gap-x-16px>
+            <input c-input-text type="text" placeholder='六位数字' />
+            <button c-btn>发送验证码</button>
+          </div>
+        </div>
+        <div mt-100px>
+          <button c-btn type="submit">登录</button>
+        </div>
+      </form>
     </div>
   )
 }
