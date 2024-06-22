1f26b908792f2c0cde04f65473e4ad23c9c4afb3
webpanck <3500146682@qq.com>
2024-06-22 19:35:34 +0800

    创建登录页面

--- src/pages/ItemsPage.tsx
+++ src/pages/ItemsPage.tsx
@@ -1,4 +1,3 @@
-import styled from 'styled-components'
 import { useState } from 'react'
 import { TopNav } from '../components/TopNav'
 import type { TimeRange } from '../components/TimeRangePicker'
@@ -6,22 +5,22 @@ import { TimeRangePicker } from '../components/TimeRangePicker'
 import { AddItemFloatButton } from '../components/AddItemFloatButton'
 import { useMenuStore } from '../stores/useMenuStore'
 import { TopMenu } from '../components/TopMenu'
+import { Gradient } from '../components/Gradient'
+import { Icon } from '../components/Icon'
 import { ItemsSummary } from './ItemsPage/ItemsSummary'
 import { ItemsList } from './ItemsPage/ItemsList'
-
-const Div = styled.div`
-  background: linear-gradient(0deg, rgba(143,76,215,1) 0%, rgba(92,51,190,1) 100%);
-`
 
 export const ItemsPage: React.FC = () => {
   const [timeRange, setTimeRange] = useState<TimeRange>('thisMonth')
   const { visible, setVisible } = useMenuStore()
   return (
     <div>
-      <Div>
-        <TopNav />
+      <Gradient>
+        <TopNav title="账目列表" icon={
+          <Icon name="menu" className="w-24px h-24px" onClick={() => { setVisible(!visible) }} />
+        } />
         <TimeRangePicker selected={timeRange} onSelected={setTimeRange} />
-      </Div>
+      </Gradient>
       <ItemsSummary />
       <ItemsList />
       <AddItemFloatButton />
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -7,6 +7,7 @@ import { Welcome3 } from '../pages/Welcome3'
 import { Welcome4 } from '../pages/Welcome4'
 import { Home } from '../pages/Home'
 import { ItemsPage } from '../pages/ItemsPage'
+import { SignInPage } from '../pages/SignInPage'
 
 export const router = createBrowserRouter([
   {
@@ -28,7 +29,7 @@ export const router = createBrowserRouter([
     ]
   },
   { path: '/items', element: <ItemsPage /> },
-  { path: '/sign_in', element: <div>sign in</div> },
+  { path: '/sign_in', element: <SignInPage /> },
   { path: '/chart', element: <div>图表</div> },
   { path: '/export', element: <div>敬请期待</div> },
   { path: '/tags', element: <div>标签</div> },
