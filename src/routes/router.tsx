941669df44c0154c72b3aae4db0cbd365d492ab3
webpanck <3500146682@qq.com>
2024-06-30 21:35:07 +0800

    创建 新建标签页面

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -9,6 +9,7 @@ import { Home } from '../pages/Home'
 import { ItemsPage } from '../pages/ItemsPage'
 import { SignInPage } from '../pages/SignInPage'
 import { ItemsNewPage } from '../pages/ItemsNewPage'
+import { TagsNewPage } from '../pages/TagsNewPage'
 
 export const router = createBrowserRouter([
   {
@@ -31,6 +32,7 @@ export const router = createBrowserRouter([
   },
   { path: '/items', element: <ItemsPage /> },
   { path: '/items/new', element: <ItemsNewPage /> },
+  { path: '/tags/new', element: <TagsNewPage /> },
   { path: '/sign_in', element: <SignInPage /> },
   { path: '/chart', element: <div>图表</div> },
   { path: '/export', element: <div>敬请期待</div> },
