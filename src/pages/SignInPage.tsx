98fcbe7a9007ef437a8b6a2f35d39a718cea864a
webpanck <3500146682@qq.com>
2024-06-30 23:43:49 +0800

    封装 Input 组件

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -6,6 +6,7 @@ import { TopNav } from '../components/TopNav'
 import { useSignInStore } from '../stores/useSignInStore'
 import { hasError, validate } from '../lib/validate'
 import { ajax } from '../lib/ajax'
+import { Input } from '../components/Input'
 
 export const SignInPage: React.FC = () => {
   const { data, error, setData, setError } = useSignInStore()
@@ -34,11 +35,9 @@ export const SignInPage: React.FC = () => {
         <h1 text-32px text="#7878FF" font-bold>山竹记账</h1>
       </div>
       <form c-form onSubmit={onSubmit}>
-        <div>
-          <span c-form-label>邮箱地址 {error.email?.[0] && <span text-red>{error.email[0]}</span>}</span>
-          <input c-input-text type="text" placeholder='请输入邮箱，然后点击发送验证码'
-            value={data.email} onChange={e => setData({ email: e.target.value })} />
-        </div>
+        <Input label='邮箱地址' placeholder='请输入邮箱，然后点击发送验证码'
+          value={data.email} onChange={value => setData({ email: value })}
+          error={error.email?.[0]} />
         <div>
           <span c-form-label>验证码 {error.code?.[0] && <span text-red>{error.code[0]}</span>}</span>
           <div flex gap-x-16px>
