61d02e8e5228c49d37ea8109df5c2da8edc506d1
webpanck <3500146682@qq.com>
2024-07-01 23:04:03 +0800

    创建「统计图表」页面

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -11,6 +11,7 @@ import { SignInPage } from '../pages/SignInPage'
 import { ItemsNewPage } from '../pages/ItemsNewPage'
 import { TagsNewPage } from '../pages/TagsNewPage'
 import { TagsEditPage } from '../pages/TagsEditPage'
+import { StatisticsPage } from '../pages/StatisticsPage'
 
 export const router = createBrowserRouter([
   {
@@ -36,7 +37,7 @@ export const router = createBrowserRouter([
   { path: '/tags/new', element: <TagsNewPage /> },
   { path: '/tags/:id', element: <TagsEditPage /> },
   { path: '/sign_in', element: <SignInPage /> },
-  { path: '/chart', element: <div>图表</div> },
+  { path: '/statistics', element: <StatisticsPage /> },
   { path: '/export', element: <div>敬请期待</div> },
   { path: '/tags', element: <div>标签</div> },
   { path: '/noty', element: <div>敬请期待</div> }
