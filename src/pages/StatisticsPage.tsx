7c33d3154903b70ccf99263c9742884a21c780e3
webpanck <3500146682@qq.com>
2024-07-01 23:32:43 +0800

    创建 LineChart 组件

--- src/pages/StatisticsPage.tsx
+++ src/pages/StatisticsPage.tsx
@@ -4,9 +4,15 @@ import { Icon } from '../components/Icon'
 import type { TimeRange } from '../components/TimeRangePicker'
 import { TimeRangePicker } from '../components/TimeRangePicker'
 import { TopNav } from '../components/TopNav'
+import { LineChart } from '../components/LineChart'
 
 export const StatisticsPage: React.FC = () => {
   const [timeRange, setTimeRange] = useState<TimeRange>('thisMonth')
+  const items = [
+    { date: '2000-01-01', value: 15000 },
+    { date: '2000-01-02', value: 25000 },
+    { date: '2000-01-31', value: 10000 },
+  ].map(item => ({ x: item.date, y: item.value }))
   return (
     <div>
       <Gradient>
@@ -15,6 +21,7 @@ export const StatisticsPage: React.FC = () => {
         } />
       </Gradient>
       <TimeRangePicker selected={timeRange} onSelect={setTimeRange} />
+      <LineChart className="h-400px" items={items} />
     </div>
   )
 }
