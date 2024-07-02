233106ac5ca9438889908a49e8a5b7915a2ed1c0
webpanck <3500146682@qq.com>
2024-07-03 00:11:20 +0800

    添加 Select 选项

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,28 +2,41 @@ import { ReactNode } from 'react'
 import { EmojiInput } from './Input/EmojiInput'
 
 type Props = {
-  label: string | ReactNode
+  label?: string | ReactNode
   placeholder?: string
-  type?: 'text' | 'emoji' | 'sms_code'
   value?: string
   onChange?: (value: string) => void
   error?: string
-}
+  disableError?: boolean
+} & (
+    | { type: 'text' }
+    | { type: 'emoji' }
+    | { type: 'sms_code' }
+    | { type: 'select'; options: { value: string; text: string }[] }
+  )
 export const Input: React.FC<Props> = (props) => {
-  const { label, placeholder, type = 'text', value, onChange, error } = props
+  const { label, placeholder, type, value, onChange, error, disableError } = props
   const renderInput = () => {
-    switch (type) {
+    switch (props.type) {
+      case undefined:
       case 'text':
         return <input c-input-text type={type} placeholder={placeholder}
           value={value} onChange={e => onChange?.(e.target.value)} />
       case 'emoji':
         return <EmojiInput value={value} onChange={value => onChange?.(value)} />
+      case 'select':
+        return <select value={value} onChange={e => onChange?.(e.target.value)}
+          className="h-36px">
+          {props.options.map(option =>
+            <option key={option.value} value={option.value}>{option.text}</option>)
+          }
+        </select>
       case 'sms_code':
         return (
           <div flex gap-x-16px>
-            <input shrink-1 j-input-text type="text" placeholder={placeholder} max-w="[calc(40%-8px)]"
+            <input shrink-1 c-input-text type="text" placeholder={placeholder} max-w="[calc(40%-8px)]"
               value={value} onChange={e => onChange?.(e.target.value)} />
-            <button max-w="[calc(60%-8px)]" shrink-0 j-btn>发送验证码</button>
+            <button max-w="[calc(60%-8px)]" shrink-0 c-btn>发送验证码</button>
           </div>
         )
       default:
@@ -33,9 +46,9 @@ export const Input: React.FC<Props> = (props) => {
   return (
     <>
       <div flex flex-col gap-y-8px>
-        <span text-18px>{label}</span>
+        {label ? <span text-18px>{label}</span> : null}
         {renderInput()}
-        <span text-red text-12px>{error || '　'}</span>
+        {disableError ? null : <span text-red text-12px>{error || '　'}</span>}
       </div>
     </>
   )
