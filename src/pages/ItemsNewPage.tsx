44a9758f6ce5af4377bfac51d486284ea4e98a2b
webpanck <3500146682@qq.com>
2024-07-07 00:13:55 +0800

    校验并提交表单数据（新建item表单）

--- src/pages/ItemsNewPage.tsx
+++ src/pages/ItemsNewPage.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useState } from 'react'
+import { ReactNode } from 'react'
 import { Gradient } from '../components/Gradient'
 import { Icon } from '../components/Icon'
 import { TopNav } from '../components/TopNav'
@@ -8,6 +8,8 @@ import { Tags } from './ItemsNewPage/Tags'
 import { ItemAmount } from './ItemsNewPage/ItemAmount'
 import { useCreateItemStore } from '../stores/useCreateItemStore'
 import { ItemDate } from './ItemsNewPage/ItemDate'
+import { useAjax } from '../lib/ajax'
+import { validate, hasError } from '../lib/validate'
 
 export const ItemsNewPage: React.FC = () => {
   const tabItems: { key: Item['kind']; text: string; element?: ReactNode }[] = [
@@ -15,8 +17,23 @@ export const ItemsNewPage: React.FC = () => {
     { key: 'income', text: '收入', element: <Tags kind="income" /> }
   ]
   const { data, error, setData, setError } = useCreateItemStore()
-  const onSubmit = () => {
-    console.log('你要提交是吧')
+  const { post } = useAjax({ showLoading: true, handleError: true })
+  const onSubmit = async () => {
+    const error = validate(data, [
+      { key: 'kind', type: 'required', message: '请选择类型：收入或支出' },
+      { key: 'tag_ids', type: 'required', message: '请选择一个标签' },
+      { key: 'happen_at', type: 'required', message: '请选择一个时间' },
+      { key: 'amount', type: 'required', message: '请输入金额' },
+      { key: 'amount', type: 'notEqual', value: 0, message: '金额不能为0' },
+    ])
+    setError(error)
+    if (hasError(error)) {
+      const message = Object.values(error).flat().join('\n')
+      window.alert(message)
+    } else {
+      const response = await post<Resource<Item>>('/api/v1/items', data)
+      console.log(response.data.resource)
+    }
   }
   return (
     <div className={s.wrapper} h-screen flex flex-col onSubmit={onSubmit}>
@@ -27,7 +44,6 @@ export const ItemsNewPage: React.FC = () => {
         classPrefix='itemsNewPage'
         value={data.kind!}
         onChange={(tabItem) => { setData({ kind: tabItem }) }} />
-      <div text-28px>{JSON.stringify(data)}</div>
       <ItemAmount className="grow-0 shrink-0" itemDate={
         <ItemDate value={data.happen_at} onChange={(happen_at) => setData({ happen_at })} />
       } value={data.amount} onChange={amount => setData({ amount })} onSubmit={onSubmit} />
