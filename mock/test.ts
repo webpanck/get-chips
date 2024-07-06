4b87976d43cce01f08327c68a5e774a1dd32d50d
webpanck <3500146682@qq.com>
2024-07-07 00:51:13 +0800

    获取 tags 的第一页数据

--- mock/test.ts
+++ mock/test.ts
@@ -2,9 +2,11 @@ import type { MockMethod } from 'vite-plugin-mock'
 import { itemsMock } from './items.mock'
 import { meMock } from './me.mock'
 import { sessionMock } from './session.mock'
+import { tagsMock } from './tags.mock'
 
 export default [
   meMock,
   itemsMock,
-  sessionMock
+  sessionMock,
+  tagsMock
 ] as MockMethod[]
