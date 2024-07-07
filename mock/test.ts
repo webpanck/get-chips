fe85ebb24669d460e42d7d49e2b331436e5ec3ca
webpanck <3500146682@qq.com>
2024-07-07 22:12:23 +0800

    items 不再是写死的，而是从后端获取的

--- mock/test.ts
+++ mock/test.ts
@@ -3,10 +3,12 @@ import { itemsMock } from './items.mock'
 import { meMock } from './me.mock'
 import { sessionMock } from './session.mock'
 import { tagsMock } from './tags.mock'
+import { summaryMock } from './summary.mock'
 
 export default [
   ...meMock,
   ...itemsMock,
   ...sessionMock,
-  ...tagsMock
+  ...tagsMock,
+  ...summaryMock
 ] as MockMethod[]
