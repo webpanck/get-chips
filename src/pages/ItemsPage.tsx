a00e27fe425c8349a7ee76916ff696eb05243970
webpanck <3500146682@qq.com>
2024-07-08 23:58:11 +0800

    处理自定义时间的边界情况

--- src/pages/ItemsPage.tsx
+++ src/pages/ItemsPage.tsx
@@ -9,14 +9,24 @@ import { Gradient } from '../components/Gradient'
 import { Icon } from '../components/Icon'
 import { ItemsSummary } from './ItemsPage/ItemsSummary'
 import { ItemsList } from './ItemsPage/ItemsList'
-import { time } from '../lib/time'
+import { Time, time } from '../lib/time'
 
 export const ItemsPage: React.FC = () => {
-  const [timeRange, setTimeRange] = useState<TimeRange>({
+  const [timeRange, _setTimeRange] = useState<TimeRange>({
     name: 'thisMonth',
     start: time().firstDayOfMonth,
     end: time().lastDayOfMonth.add(1, 'day')
   })
+  const [outOfRange, setOutOfRange] = useState(false)
+  const setTimeRange = (t: TimeRange) => {
+    if (t.start.timestamp > t.end.timestamp) {
+      [t.start, t.end] = [t.end, t.start]
+    }
+    if (t.end.timestamp - t.start.timestamp > Time.DAY * 365) {
+      setOutOfRange(true)
+    }
+    _setTimeRange(t)
+  }
   const { visible, setVisible } = useMenuStore()
   const { start, end } = timeRange
 
@@ -28,11 +38,15 @@ export const ItemsPage: React.FC = () => {
         } />
       </Gradient>
       <TimeRangePicker selected={timeRange} onSelect={setTimeRange} />
-      <div>
-        {start.isoString} | {end.isoString}
-      </div>
-      <ItemsSummary />
-      <ItemsList start={start} end={end} />
+      {outOfRange
+        ? <div text-center p-32px>
+          自定义时间跨度不能超过 365 天
+        </div>
+        : <>
+          <ItemsSummary />
+          <ItemsList start={start} end={end} />
+        </>
+      }
       <AddItemFloatButton />
       <TopMenu visible={visible} onClickMask={() => { setVisible(false) }} />
     </div>
